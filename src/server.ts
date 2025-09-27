6ac82351626bed41cd0b2fa98a28d77234448a84
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-27 00:39:37 -0300

    feat(api): add users routers in server

--- src/server.ts
+++ src/server.ts
@@ -2,6 +2,7 @@ import express from "express";
 import { env } from "../env.ts";
 import cors from "cors";
 import { connectRedis } from "./config/redis.ts";
+import usersRoutes from "./routes/users.route.ts";
 
 const app = express();
 
@@ -9,6 +10,8 @@ app.use(cors());
 app.use(express.json());
 
 const PORT = env.PORT;
+
+app.use("/api/users", usersRoutes);
 
 const startup = async () => {
   await connectRedis();
