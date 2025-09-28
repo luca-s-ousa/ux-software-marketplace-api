e115bcbd1af42e1ccb7ca57b46d12204eed54d94
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-28 17:41:12 -0300

    feat(api): add middleware in route register new user

--- src/routes/users.route.ts
+++ src/routes/users.route.ts
@@ -1,10 +1,22 @@
 import express from "express";
 import { registerNewUser } from "../controllers/users.controller.ts";
+import { validate } from "../middlewares/zod.middleware.ts";
+import { registerUserSchema } from "../schemas/users.ts";
+import {
+  alreadyExistsUserWithCpf,
+  alreadyExistsUserWithEmail,
+} from "../middlewares/users.middleware.ts";
 
 const router = express.Router();
 
 // POST /api/users
 // Criar Usuarios
-router.post("/", registerNewUser);
+router.post(
+  "/",
+  validate(registerUserSchema),
+  alreadyExistsUserWithEmail,
+  alreadyExistsUserWithCpf,
+  registerNewUser
+);
 
 export default router;
