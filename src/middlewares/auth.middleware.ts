0107fa29606e86e1857cb81c0f2202c6612cbefc
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-28 10:46:54 -0300

    feat(api): add middleware auth guard

---
+++ src/middlewares/auth.middleware.ts
@@ -0,0 +1,39 @@
+import express from "express";
+import jwt from "jsonwebtoken";
+// import { TokenExpiredError } from "jsonwebtoken";
+import type { UserRequest } from "../types/users.ts";
+import { env } from "../schemas/env.ts";
+
+export const authGuard = (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const header = req.headers.authorization;
+  if (!header) {
+    return res.status(401).json({ success: false, message: "Token inválido" });
+  }
+
+  try {
+    const token = header.split(" ")[1];
+    const payload = jwt.verify(token, env.JWT_SECRET as string) as UserRequest;
+
+    req.user = {
+      id: payload.id,
+      email: payload.email,
+      isAdmin: payload.isAdmin,
+    };
+    next();
+  } catch (err) {
+    // if (err instanceof TokenExpiredError) {
+    //   return res.status(401).json({
+    //     success: false,
+    //     message: "Sessão expirada",
+    //   });
+    // }
+    return res.status(401).json({
+      success: false,
+      message: "Token inválido",
+    });
+  }
+};
