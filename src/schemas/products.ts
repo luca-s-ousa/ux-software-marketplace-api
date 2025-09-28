6d018999e4019a5edfd17229ede90ef4da50977f
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-28 18:18:12 -0300

    feat: schema register new product implemented

---
+++ src/schemas/products.ts
@@ -0,0 +1,14 @@
+import z from "zod";
+
+export const addNewProductSchema = z.object({
+  name: z
+    .string("Você precisa informar um nome para o produto!")
+    .min(2, "O nome do produto deve ter pelo menos 2 caracteres!"),
+  description: z
+    .string()
+    .max(500, "A descrição deve ter no máximo somente 500 caracteres!")
+    .nullable()
+    .optional(),
+  price: z.number("Preço inválido!").nonnegative("Preço inválido"),
+  stock: z.number("Estoque inválido!").nonnegative("Estoque inválido!"),
+});
