ecd1eb49f3542f8a147454324f022930ffccfee1
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-28 10:36:55 -0300

    chore(api): add types users

---
+++ src/types/users.ts
@@ -0,0 +1,13 @@
+export type User = {
+  id?: string;
+  name: string;
+  email: string;
+  cpf: string;
+  phone: string | null;
+  password: string;
+  isAdmin: boolean;
+  isActive: boolean;
+  createdAt: string;
+};
+
+export type UserRequest = Pick<User, "id" | "email" | "isAdmin">;
