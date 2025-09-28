256e3683ce9f97c4689565f0e73c550205929bdd
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-28 18:18:56 -0300

    feat: add in route register new product validate schema new product

--- src/routes/products.route.ts
+++ src/routes/products.route.ts
@@ -3,12 +3,21 @@ import { authGuard } from "../middlewares/auth.middleware.ts";
 import { adminOnly } from "../middlewares/users.middleware.ts";
 import multer from "multer";
 import { addNewProduct } from "../controllers/products.controller.ts";
+import { addNewProductSchema } from "../schemas/products.ts";
+import { validate } from "../middlewares/zod.middleware.ts";
 
 const router = express.Router();
 
 const upload = multer({ storage: multer.memoryStorage() });
 
 // POST /api/products
-router.post("/", authGuard, adminOnly, upload.single("file"), addNewProduct);
+router.post(
+  "/",
+  upload.single("file"),
+  validate(addNewProductSchema),
+  authGuard,
+  adminOnly,
+  addNewProduct
+);
 
 export default router;
