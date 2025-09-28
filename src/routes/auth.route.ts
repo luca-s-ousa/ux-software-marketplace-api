e3a914cd5f280ea9bdb0fef6a3d9a7c0d60d055c
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-28 17:44:50 -0300

    feat: add in route login validate schema login

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -1,10 +1,12 @@
 import express from "express";
 import { login } from "../controllers/auth.controller.ts";
+import { validate } from "../middlewares/zod.middleware.ts";
+import { loginSchema } from "../schemas/auth.ts";
 
 const router = express.Router();
 
 // POST /api/auth/login
 // Rota de Login de Usuários
-router.post("/login", login);
+router.post("/login", validate(loginSchema), login);
 
 export default router;
