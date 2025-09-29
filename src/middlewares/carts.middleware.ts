8ffb234f5edfe6028ae9b9922ef2d6a5af88e1fc
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-29 18:18:19 -0300

    feat: validate existing item in cart implemented

--- src/middlewares/carts.middleware.ts
+++ src/middlewares/carts.middleware.ts
@@ -1,6 +1,6 @@
 import express from "express";
 import { db } from "../config/db.ts";
-import { cartsTable } from "../../drizzle/schema.ts";
+import { cartItemsTable, cartsTable } from "../../drizzle/schema.ts";
 import { eq } from "drizzle-orm";
 import type { Cart } from "../types/carts.ts";
 
@@ -36,6 +36,43 @@ export const validateExistingCart = async (
   const cartDto: Cart = { ...cart, createdAt: cart.createdAt.toISOString() };
 
   req.cart = cartDto;
+
+  next();
+};
+
+export const validateExistingItemInCart = async (
+  req: express.Request,
+  res: express.Response,
+  next: express.NextFunction
+) => {
+  const { id } = req.params;
+
+  if (
+    !id ||
+    !id.match(
+      /^[0-9a-fA-F]{8}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{4}-[0-9a-fA-F]{12}$/
+    )
+  ) {
+    return res.status(400).json({
+      success: false,
+      message: "ID do item inválido",
+    });
+  }
+
+  const [cartItem] = await db
+    .select()
+    .from(cartItemsTable)
+    .where(eq(cartItemsTable.id, id))
+    .limit(1);
+
+  if (!cartItem) {
+    return res.status(404).json({
+      success: false,
+      message: "Item não encontrado no carrinho",
+    });
+  }
+
+  req.cartItem = cartItem;
 
   next();
 };
