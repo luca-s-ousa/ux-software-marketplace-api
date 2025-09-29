a27e17a3ed5871d19dac3f1a79aef8425f400efd
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-29 17:35:51 -0300

    feat: add controller in router add/update product in cart

--- src/routes/carts.route.ts
+++ src/routes/carts.route.ts
@@ -1,9 +1,28 @@
 import express from "express";
 import { authGuard } from "../middlewares/auth.middleware.ts";
-import { getAllItensCart } from "../controllers/carts.controller.ts";
+import {
+  addItemToCart,
+  getAllItensCart,
+} from "../controllers/carts.controller.ts";
+import { validate } from "../middlewares/zod.middleware.ts";
+import { createOrUpdateCartItemsSchema } from "../schemas/carts.ts";
+import {
+  normalizeNumberCartItems,
+  validateExistingCart,
+} from "../middlewares/carts.middleware.ts";
+import { validateStockProduct } from "../middlewares/product.middleware.ts";
 
 const router = express.Router();
 
 router.get("/", authGuard, getAllItensCart);
+router.post(
+  "/items",
+  authGuard,
+  normalizeNumberCartItems,
+  validate(createOrUpdateCartItemsSchema),
+  validateExistingCart,
+  validateStockProduct,
+  addItemToCart
+);
 
 export default router;
