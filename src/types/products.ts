382c8310d861959f751737aa6c38f546b483e32e
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-29 10:47:17 -0300

    chore: change type product

--- src/types/products.ts
+++ src/types/products.ts
@@ -1,12 +1,12 @@
 export type Product = {
-  id?: string;
+  id?: string | null;
   name: string;
-  imgUrl?: string;
-  description?: string;
+  imgUrl?: string | null;
+  description?: string | null;
   price: number;
   stock: number;
   createdAt: string;
-  categorieId: string;
+  categorieId?: string | null;
 };
 
 export type ProductBodyParams = Omit<Product, "id" | "imgUrl" | "createdAt">;
