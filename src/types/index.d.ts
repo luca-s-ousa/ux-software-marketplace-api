a824740572c11da6fa53c4d700d1d614089a2124
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-29 10:48:06 -0300

    chore: add global type product

--- src/types/index.d.ts
+++ src/types/index.d.ts
@@ -1,9 +1,11 @@
+import { Product } from "./products.ts";
 import { UserRequest } from "./users.ts"; // ajuste o caminho para o seu User
 
 declare global {
   namespace Express {
     export interface Request {
-      user?: UserRequest; // agora o TS sabe que existe req.user
+      user?: UserRequest;
+      product?: Product;
     }
   }
 }
