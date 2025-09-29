ef0e352e8dcd4fd65b6eaafbf536298d364630f3
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-29 14:21:26 -0300

    feat: schema validate update product implemented

--- src/schemas/products.ts
+++ src/schemas/products.ts
@@ -12,3 +12,26 @@ export const addNewProductSchema = z.object({
   price: z.number("Preço inválido!").nonnegative("Preço inválido"),
   stock: z.number("Estoque inválido!").nonnegative("Estoque inválido!"),
 });
+
+export const updateProductSchema = z.object({
+  name: z
+    .string()
+    .min(2, "O nome do produto deve ter pelo menos 2 caracteres!")
+    .nullable()
+    .optional(),
+  description: z
+    .string()
+    .max(500, "A descrição deve ter no máximo somente 500 caracteres!")
+    .nullable()
+    .optional(),
+  price: z
+    .number("Preço inválido!")
+    .nonnegative("Preço inválido")
+    .nullable()
+    .optional(),
+  stock: z
+    .number("Estoque inválido!")
+    .nonnegative("Estoque inválido!")
+    .nullable()
+    .optional(),
+});
