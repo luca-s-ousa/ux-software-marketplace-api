bdde471d0cbb5820bf58e97144a912d24e237046
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-29 21:05:11 -0300

    feat: add controller confirm user in route get confirm user

--- src/routes/auth.route.ts
+++ src/routes/auth.route.ts
@@ -2,11 +2,17 @@ import express from "express";
 import { login } from "../controllers/auth.controller.ts";
 import { validate } from "../middlewares/zod.middleware.ts";
 import { loginSchema } from "../schemas/auth.ts";
+import {
+  validateEmailQuery,
+  validateTokenQuery,
+} from "../middlewares/users.middleware.ts";
+import { confirmUser } from "../controllers/users.controller.ts";
 
 const router = express.Router();
 
 // POST /api/auth/login
 // Rota de Login de Usuários
 router.post("/login", validate(loginSchema), login);
+router.get("/confirm", validateEmailQuery, validateTokenQuery, confirmUser);
 
 export default router;
