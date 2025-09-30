3bd63841c7eda3cb879591d6f111162dccea58f7
Lucas Sousa <lsousatop5771@gmail.com>
2025-09-29 21:09:58 -0300

    feat: controller confirm user implemented

--- src/controllers/users.controller.ts
+++ src/controllers/users.controller.ts
@@ -10,6 +10,7 @@ import { redis } from "../config/redis.ts";
 import { env } from "../schemas/env.ts";
 import { sendWebhook } from "../services/send.email.ts";
 import { createCart } from "../utils/carts.ts";
+import type { UserRequest } from "../types/users.ts";
 
 export const registerNewUser = async (
   req: express.Request,
@@ -30,20 +31,18 @@ export const registerNewUser = async (
 
     const token = uuidv4();
 
-    const campos = { cpf, name, email, token };
-
+    const campos = { email, token };
     const dados = stringifyJson(campos, "stringify");
 
-    const key = `${name as string}:${cpf as string}`;
+    const keyAccessToken = token as string;
 
-    await redis.hSet(key, dados as Record<string, string>);
-
-    await redis.expire(key, Number(env.EXPIRE_TIME_REDIS) || 3600);
+    await redis.hSet(keyAccessToken, dados as Record<string, string>);
+    await redis.expire(keyAccessToken, Number(env.EXPIRE_TIME_REDIS) || 3600);
 
     const responseSendEmail = await sendWebhook({
       email,
       name,
-      link: `http://localhost:3000/api/auth/confirm?token=${token}`,
+      link: `${env.ENDPOINT_API}/api/auth/confirm?token=${token}&email=${email}`,
     });
 
     return res.status(200).json({
@@ -53,6 +52,65 @@ export const registerNewUser = async (
     });
   } catch (error) {
     console.error("Erro ao registrar o usuario: ", error);
+    return res.status(500).json({
+      success: false,
+      message: "Erro Inesperado",
+    });
+  }
+};
+
+export const confirmUser = async (
+  req: express.Request,
+  res: express.Response
+) => {
+  try {
+    const token = req.query.token as string;
+    const { email: emailUser, name } = req.user as UserRequest;
+
+    const userData = await redis.hGetAll(token);
+
+    if (!userData || Object.keys(userData).length === 0) {
+      const newToken = uuidv4();
+      const campos = { email: emailUser, token: newToken };
+      const dados = stringifyJson(campos, "stringify");
+
+      const keyAccessToken = newToken as string;
+
+      await redis.hSet(keyAccessToken, dados as Record<string, string>);
+      await redis.expire(keyAccessToken, Number(env.EXPIRE_TIME_REDIS) || 3600);
+
+      const responseSendEmail = await sendWebhook({
+        email: emailUser,
+        name,
+        link: `${env.ENDPOINT_API}/api/auth/confirm?token=${newToken}&email=${emailUser}`,
+      });
+      return res.status(400).json({
+        success: true,
+        message: "Enviamos um novo link de confirmação para seu email",
+        data: { ...responseSendEmail },
+      });
+    }
+
+    const { email } = userData;
+
+    const [updatedUser] = await db
+      .update(usersTable)
+      .set({ isActive: true })
+      .where(eq(usersTable.email, email))
+      .returning();
+
+    await redis.del(token);
+
+    return res.status(200).json({
+      success: true,
+      message: "Conta confirmada com sucesso",
+      data: {
+        id: updatedUser.id,
+        name: updatedUser.name,
+        email: updatedUser.email,
+      },
+    });
+  } catch (error) {
     return res.status(500).json({
       success: false,
       message: "Erro Inesperado",
